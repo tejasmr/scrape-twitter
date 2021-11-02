0ee9f60afbeab9ddd1d17602cb7388eed75507e9
Tejas M R <matblockgreninja@gmail.com>
2021-11-02 06:24:19 +0000

    Added pandas

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
+python3.8 -m pip install pandas
 python3.8 -m pip install  git+https://github.com/JustAnotherArchivist/snscrape.git
 rm -rf filename.txt
 wget https://raw.githubusercontent.com/tezz-io/ds_lab/main/output/filename.txt
