337914d840c65fe755c29af270833ecc67b634de
Tejas M R <52408360+tezz-io@users.noreply.github.com>
2021-11-02 15:02:54 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 apt install python3.8
+apt install python3.8-distutils
+wget https://bootstrap.pypa.io/get-pip.py
+python3.8 get-pip.py
 python3.8 -m pip install pandas
 python3.8 -m pip install  git+https://github.com/JustAnotherArchivist/snscrape.git
 rm -rf filename.txt
