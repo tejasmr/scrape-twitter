e9acd53f77d346046c22b105bd1eaf68fce6c9ab
Tejas M R <matblockgreninja@gmail.com>
2021-11-02 06:16:28 +0000

    created install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+python3.8 -m pip install  git+https://github.com/JustAnotherArchivist/snscrape.git
+rm -rf filename.txt
+wget https://raw.githubusercontent.com/tezz-io/ds_lab/main/output/filename.txt
